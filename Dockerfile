98d44009483b9a9c128292633b1848eba4081437
Eduardo Fonseca <eduardofesilva@gmail.com>
2020-04-20 15:33:47 -0300

    Included new verifications during the entrypoint

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,10 @@
 FROM golang:alpine
 MAINTAINER "Eduardo Maldonado Fonseca Silva"
-
 ENV PACKER_DEV=1
-
 RUN apk add --update git bash openssl ansible
-
 RUN go get github.com/mitchellh/gox
 RUN go get github.com/hashicorp/packer
-
-WORKDIR $GOPATH/src/github.com/hashicorp/packer
-
-COPY . $GOPATH
-
-RUN /bin/bash scripts/build.sh
-
-WORKDIR $GOPATH
-ENTRYPOINT ["/entrypoint.sh"]
+COPY . .
+RUN /bin/bash $GOPATH/src/github.com/hashicorp/packer/scripts/build.sh
+ENTRYPOINT ["./entrypoint.sh"]
 #ENTRYPOINT ["bin/packer"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,19 @@
-#!/bin/sh
-PACKERFILE=$GOPATH/$1
+#!/bin/sh -l
+
+# Validating if packer and ansible playbook exist
+[ -z "$INPUT_PACKERFILE" ] && return 1 || echo "PACKER FILE = $INPUT_PACKERFILE"
+[ -z "$INPUT_PLAYBOOKFILE" ] && return 1 || echo "PLAYBOOK FILE = $INPUT_PLAYBOOKFILE"
+
+export PACKERFILE=$GOPATH/$INPUT_PACKERFILE
+export PLAYBOOK=$GOPATH/$INPUT_PLAYBOOKFILE
+
+echo "PACKER FILE = $PACKERFILE"
+echo "PLAYBOOK FILE = $PLAYBOOK"
+
 REQUIREMENTS=$GOPATH/requirements.txt
 if [ -f "$REQUIREMENTS" ]; then
     ansible-galaxy install -r $REQUIREMENTS
 else 
     AMI=$($GOPATH/bin/packer build -machine-readable $PACKER_FILE | awk -F, '$0 ~/artifact,0,id/ {print $6}')
-    echo "::set-output name=ami::$tAMI"
+    echo "::set-output name=ami::$AMI"
 fi
